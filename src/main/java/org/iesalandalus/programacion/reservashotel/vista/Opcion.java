3c8ba8e07c56c0858cb92e31e2d6fe476fa2ca74
Luis_Miguel <lumisamo_1983@hotmail.com>
2024-01-23 21:15:56 +0100

    Clase opcion con checkin y checkout.

--- src/main/java/org/iesalandalus/programacion/reservashotel/vista/Opcion.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/vista/Opcion.java
@@ -7,14 +7,18 @@ public enum Opcion {
     BUSCAR_HUESPED("Buscar huesped"),
     BORRAR_HUESPED("Borrar Huesped"),
     MOSTRAR_HUESPEDES("Mostrar Huesped"),
-    INSERTAR_HABITACION("Insertar Habitación"),
-    BUSCAR_HABITACION("Buscar habitación"),
-    BORRAR_HABITACION("Borrar Habitación"),
+    INSERTAR_HABITACION("Insertar Habitaciï¿½n"),
+    BUSCAR_HABITACION("Buscar habitaciï¿½n"),
+    BORRAR_HABITACION("Borrar Habitaciï¿½n"),
     MOSTRAR_HABITACIONES("Mostrar Habitaciones"),
     INSERTAR_RESERVA("Insertar Reserva"),
     ANULAR_RESERVA("Anular Reserva"),
     MOSTRAR_RESERVAS("Mostrar Reserva"),
-    CONSULTAR_DISPONIBILIDAD("Consultar disponibilidad");
+    CONSULTAR_DISPONIBILIDAD("Consultar disponibilidad"),
+
+
+    REALIZAR_CHECKIN("Realizar checkin"),
+    REALIZAR_CHECKOUT("Realizar checkout");
 
 
     private String mensajeAMostrar;
